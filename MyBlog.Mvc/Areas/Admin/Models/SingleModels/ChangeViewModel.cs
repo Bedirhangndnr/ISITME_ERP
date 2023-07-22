c18027dc98a4fe42bf6ad0268d02625bbbca9732
Bedirhan <bedirhangndnr@gmail.com>
2023-07-22 18:05:13 +0300

    DeleteAllFiles

--- MyBlog.Mvc/Areas/Admin/Models/SingleModels/ChangeViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyBlog.Mvc.Areas.Admin.Models.SingleModels
-{
-    public class ChangeViewModel
-    {
-        public string EntityName { get; set; }
-        public string ChangeType { get; set; }
-        // diğer özellikler
-    }
-}
--- MyBlog.Mvc/Areas/Admin/Models/UserModels/UserViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using MyBlog.Entities.Concrete;
-
-namespace MyBlog.Mvc.Areas.Admin.Models.UserModels
-{
-    public class UserViewModel
-    {
-        public User User { get; set; }
-    }
-}
